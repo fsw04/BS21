fcbf5b06be7401af83165205c937feea9b445db7
fsw04 <fsw04@users.noreply.github.com>
2026-06-10 18:29:15 +0000

    feat: 克隆代码
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- src/application/samples/peripheral/st7796s_ft6336u/tasks/main.c
+++ src/application/samples/peripheral/st7796s_ft6336u/tasks/main.c
@@ -1,10 +1,7 @@
 /**
  * Copyright (c) HiSilicon (Shanghai) Technologies Co., Ltd. 2024-2024. All rights reserved.
  *
- * Description: Health monitor with LVGL + SLE 1VN \n
- *
- * History: \n
- * 2024-03-04, Create file. \n
+ * Description: Health monitor with LVGL \n
  */
 #include "soc_osal.h"
 #include "securec.h"
@@ -16,25 +13,10 @@
 #include "st7796s.h"
 #include "ft6336u.h"
 #include "lcd_bus.h"
-#include "sle_1vn_server.h"
-#include "sensor_task.h"
 
 #define UI_TASK_PRIO                    24
 #define UI_TASK_STACK_SIZE              0x3000
 #define LVGL_TICK_PERIOD_MS             5
-#define SLE_STATUS_POLL_MS              500
-
-static void on_identity_received(const char *name, const char *id_number)
-{
-    osal_printk("[main] identity: %s %s\r\n", name, id_number);
-    ui_health_set_identity(name, id_number);
-}
-
-static void update_sle_status(void)
-{
-    bool connected = sle_1vn_server_is_connected();
-    ui_health_set_sle_status(connected, "watch-01", connected ? -45 : 0);
-}
 
 static void *ui_task(const char *arg)
 {
@@ -74,40 +56,10 @@ static void *ui_task(const char *arg)
     ui_health_create();
     osal_printk("[main] UI create ok\r\n");
 
-    /* 注册身份回调 */
-    wearable_register_identity_callback(on_identity_received);
-
-    /* 暂时禁用 SLE，先验证 LVGL 显示 */
-#if 0
-    /* 初始化 SLE 服务端 */
-    osal_printk("[main] SLE init...\r\n");
-    ret = sle_1vn_server_init();
-    if (ret != ERRCODE_SUCC) {
-        osal_printk("[main] SLE init failed 0x%x\r\n", ret);
-    } else {
-        osal_printk("[main] SLE init ok\r\n");
-        sensor_task_start();
-    }
-#else
-    osal_printk("[main] SLE disabled for testing\r\n");
-#endif
-
     osal_printk("[main] LVGL loop start\r\n");
 
-    uint32_t last_sle_poll = 0;
-
     for (;;) {
-        uint32_t now = osal_jiffies_to_msecs((unsigned int)osal_get_jiffies());
-
-        /* LVGL 主循环 */
         lv_timer_handler();
-
-        /* 定期更新 SLE 状态 */
-        if ((now - last_sle_poll) >= SLE_STATUS_POLL_MS) {
-            update_sle_status();
-            last_sle_poll = now;
-        }
-
         osal_msleep(LVGL_TICK_PERIOD_MS);
     }
 
